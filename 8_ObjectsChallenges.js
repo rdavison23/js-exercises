9071a619c4be7a196fb39d7cebdf000a755a4d4c
Your Name <your-email@example.com>
2025-10-14 16:16:12 -0700

    8

---
+++ 8_ObjectsChallenges.js
@@ -0,0 +1,21 @@
+// Exercise 1.
+function keys(obj) {
+    let result = [];
+    for (let key in obj) {
+      result.push(key);
+    }
+    return result;
+  }
+  
+// Exercise 2.
+function values(obj) {
+    let result = [];
+    for (let key in obj) {
+      result.push(obj[key]);
+    }
+    return result;
+  }
+  
+// Exercise 3.
+
+  
